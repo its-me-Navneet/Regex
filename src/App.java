4fabbb02de795d28824b6ea050436fe2d5339c9a
navneet kumar <20bcs142@iiitdmj.ac.in>
2023-12-28 14:25:56 +0530

    Added UC5

--- src/App.java
+++ src/App.java
@@ -5,19 +5,10 @@ public class App {
     public static void main(String[] args) throws Exception {
        
      // User Registration validation 
-
-     String name = "Navneet Kumar" ; 
-     String regex = "^[A-Z]+[A-Za-z]{2,}\\s[A-za-z]*$";
-     Pattern pattern = Pattern.compile(regex) ; 
-     Matcher matcher = pattern.matcher(name) ; 
-     boolean match = matcher.matches() ;
-     System.out.println(match); 
-
-
-     String phone = "91 7258076955";
-     String regexP = "^[9]{1}[1]{1}\\s[1-9]{1,1}[0-9]{9}$" ;
-
-     System.out.println(Pattern.matches(regexP,phone));
+    
+     String password ="xyhssabsd" ; 
+     String regex = "(.){8,}" ;
+     System.out.println(Pattern.matches(regex,password));
 
     }
 }
