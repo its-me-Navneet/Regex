747cf3c384ba9ab032059f89102fed37d1960bc3
navneet kumar <20bcs142@iiitdmj.ac.in>
2023-12-28 19:08:24 +0530

    Added UC3

--- src/App.java
+++ src/App.java
@@ -6,12 +6,19 @@ public class App {
        
      // User Registration validation 
 
-     String name = "Navneet Kumar" ; 
-     String regex = "^[A-Z]+[A-Za-z]{2,}\\s[A-za-z]*$";
-     Pattern pattern = Pattern.compile(regex) ; 
-     Matcher matcher = pattern.matcher(name) ; 
-     boolean match = matcher.matches() ;
-     System.out.println(match);
+    //  String name = "Navneet Kumar" ; 
+    //  String regex = "^[A-Z]+[A-Za-z]{2,}\\s[A-za-z]*$";
+    //  Pattern pattern = Pattern.compile(regex) ; 
+    //  Matcher matcher = pattern.matcher(name) ; 
+    //  boolean match = matcher.matches() ;
+    //  System.out.println(match); 
+
+ 
+     // char digit not all digit - @
+     String email ="xyz@ge.com" ; 
+     String regexE = "^[a-zA-Z0-9](?=[^@]*[@][^@]*$)[a-zA-z0-9@.+-]{3,}[a-zA-Z]{2,2}$" ; 
+
+     System.out.println(Pattern.matches(regexE, email));
 
     }
 }
