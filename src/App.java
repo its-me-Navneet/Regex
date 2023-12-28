9a26a1636ee22d35fd307db5302e79787204d104
navneet kumar <20bcs142@iiitdmj.ac.in>
2023-12-28 19:31:42 +0530

    Added UC9

--- src/App.java
+++ src/App.java
@@ -12,13 +12,25 @@ public class App {
     //  Matcher matcher = pattern.matcher(name) ; 
     //  boolean match = matcher.matches() ;
     //  System.out.println(match); 
-
  
+    String []valid ={"abc@yahoo.com" , "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au","abc@1.com","abc@gmail.com.com","abc+100@gmail.com"} ;
+    
      // char digit not all digit - @
      String email ="xyz@ge.com" ; 
-     String regexE = "^[a-zA-Z0-9](?=[^@]*[@][^@]*$)[a-zA-z0-9@.+-]{3,}[a-zA-Z]{2,2}$" ; 
-
-     System.out.println(Pattern.matches(regexE, email));
-
+     String regexE = "^[a-zA-Z0-9](?!.*[.][.])(?!.*[@][.])(?!.*[.][@])(?=[^@]*[@][^@]*$)[a-zA-z0-9@.+-]{3,}[a-zA-Z]{2,2}$" ; 
+ 
+      for(String Email :valid ){
+      String ans =Pattern.matches(regexE, Email)==true ? "Valid":"Invalid" ; 
+      System.out.println(Email + " is "+ ans);
+      }
+      System.out.println("************");
+    
+      String invalid[] = {"abc", "abc@.com.my","3. abc123@gmail.a","abc123@.com","abc123@.com.com",".abc@abc.com","abc()*@gmail.com","abc@%*.com","abc..2002@gmail.com","abc.@gmail.com","abc@abc@gmail.com","abc@gmail.com.1a","abc@gmail.com.aa.au"} ;
+  
+       for(String Email : invalid){
+          String ans =Pattern.matches(regexE, Email)==true ? "Valid":"Invalid" ; 
+          System.out.println(Email + " is "+ ans);
+      }
+       }
     }
-}
+
