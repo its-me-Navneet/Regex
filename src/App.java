06eaf65b5183416b708c3f312d81eab98a219202
navneet kumar <20bcs142@iiitdmj.ac.in>
2023-12-28 17:50:23 +0530

    Added UC8

--- src/App.java
+++ src/App.java
@@ -4,12 +4,16 @@ import java.util.regex.Pattern;
 public class App {
     public static void main(String[] args) throws Exception {
        
-     // User Registration validation 
-    
-     String password ="asdbs2hAasdas" ; 
+     // User Registration validation \
+
+     String password ="sadasdfds$fDs4@" ; 
      String regex = "(?=.*[A-Z])(.){8,}" ; 
-     regex ="(?=.*[0-9])" + regex  ;
-     System.out.println(Pattern.matches(regex,password));
-     
+     regex ="(?=.*[0-9])" + regex  ;  
+     // ..$......
+     regex ="(?=[a-zA-Z0-9]*[^a-zA-Z0-9][a-zA-Z0-9]*$)(?=.*[0-9])"+ regex ; 
+      System.out.println(regex);
+     System.out.println(Pattern.matches(regex,password)); 
+  
+    
     }
 }
