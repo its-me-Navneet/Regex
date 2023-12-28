6002cbbcafbc69b53678f4d9a31703a60026b897
navneet kumar <20bcs142@iiitdmj.ac.in>
2023-12-28 13:03:01 +0530

    Added UC1

--- src/App.java
+++ src/App.java
@@ -1,7 +1,17 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class App {
     public static void main(String[] args) throws Exception {
        
      // User Registration validation 
+
+     String name = "Navneet Kumar" ; 
+     String regex = "^[A-Z]+[A-Za-z]*\\s[A-za-z]*$";
+     Pattern pattern = Pattern.compile(regex) ; 
+     Matcher matcher = pattern.matcher(name) ; 
+     boolean match = matcher.matches() ;
+     System.out.println(match);
 
     }
 }
